006f72b7f73ef9e86183bbe48e3a1048da585fab
Christian Thomas <christian.thomas.2894@gmail.com>
2018-09-04 23:54:33 +0000

    automatically backed up by learn

--- spec/views/sessions/new_spec.rb
+++ spec/views/sessions/new_spec.rb
@@ -1,1 +1,9 @@
+require 'rails_helper'
 
+feature 'Add a login' do
+  scenario 'able to login and show user' do
+    visit '/'
+    click_link '/login'
+    fill_in 'name', with: 'Tim'
+    click_button 'login'
+    expect(page).to have_content('Tim')
